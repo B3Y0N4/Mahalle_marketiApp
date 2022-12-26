cecb600bea323cee56305a8f6a4d9460e397a181
Abduljalal Lawal <abduljalal.jr@gmail.com>
2022-12-26 20:17:30 +0300

    siyah ekran

--- Mahalle_marketi/Form1.Designer.cs
+++ Mahalle_marketi/Form1.Designer.cs
@@ -268,7 +268,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.panel1.Location = new System.Drawing.Point(341, 150);
+            this.panel1.Location = new System.Drawing.Point(238, 296);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
             this.panel1.TabIndex = 1;
